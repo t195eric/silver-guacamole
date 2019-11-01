9395f452ed11e0632bbf6406f1b6a21c66e029c8
Eric Whitehead <ericwhitehead77@gmail.com>
2019-11-01 17:58:44 +0000

    styled and added validations for new forms

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,8 +6,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
+    @place = current_user.places.create(place_params)
+    if @place.valid?
     redirect_to root_path
+  else
+    render :new, status: unprocessable_entity
+  end
   end
 
   def show
@@ -30,7 +34,11 @@ class PlacesController < ApplicationController
     end
 
     @place.update_attributes(place_params)
+    if @place.valid?
     redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
   end
 
   def destroy
