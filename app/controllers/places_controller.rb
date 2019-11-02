505c2f177fa96ddce6fc206486bd1b198c1b2e71
Eric Whitehead <ericwhitehead77@gmail.com>
2019-11-02 06:16:41 +0000

    FIXED ALL THE BUGS!

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,10 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
+def index
+    @places = Place.order(:name).page(params[:page]).per(10)
+  end
+
   def new
     @place = Place.new
   end
