a2634b5afcf779d8c956deedd5f6f6ff4953c01e
Eric Whitehead <ericwhitehead77@gmail.com>
2019-11-03 05:45:24 +0000

    Fixed map geocoding

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
-
+  geocoded_by :address
+  after_validation :geocode
   validates :name, length: { minimum: 3}, presence: true
   validates :description, presence: true
   validates :address, presence: true
-  geocoded_by :address
-  after_validation :geocode
+  
 end
