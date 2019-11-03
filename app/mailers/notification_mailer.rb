0f9b41c822631a27d4ff6628c79b8c8000b0881a
Eric Whitehead <ericwhitehead77@gmail.com>
2019-11-03 09:48:38 +0000

    mailer?

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
   def comment_added
-    mail(to: "my@email.com",
+    mail(to: "ericwhitehead77@gmail.com",
       subject: "A comment has been added to your place")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,20 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+
+config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+
+
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
